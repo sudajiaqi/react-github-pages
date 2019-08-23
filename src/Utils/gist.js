9820f5d28857b3f72a36fd8c905cb62e294eeebc
qi-eb <jiaqi@easternbay.cn>
2019-08-23 18:24:36 +0800

    new instace for axios

--- src/Utils/gist.js
+++ src/Utils/gist.js
@@ -4,24 +4,21 @@ import { getToken } from './cookie';
 const token = getToken();
 const baseURL = 'https://api.github.com';
 const userName = process.env.USER_NAME;
+
+const instance = axios.create({
+  baseURL,
+});
+
 export const getGists = (page = 1) => {
   const config = {
-    // Accept: 'application/vnd.github.v3+json',
-    baseURL,
     params: { page, per_page: 20 },
-    // headers: {
-    //   Authorization: 'token 3cb476c3b0281ee28bb50f4e7e5dd0d75f4d165f',
-    // },
   };
-  return axios.get(`/users/${userName}/gists`, config);
+  return instance.get(`/users/${userName}/gists`, config);
 };
 
 
 export const getGist = (id) => {
-  const config = {
-    baseURL,
-  };
-  return axios.get(`/gists/${id}`, config);
+  return instance.get(`/gists/${id}`);
 };
 
 export const updateGist = (id, title, description, content) => {
@@ -29,16 +26,15 @@ export const updateGist = (id, title, description, content) => {
     public: true,
     description,
     files: {
-      [title]: { content, filename: title },
+      [title]: { content },
     },
   };
   const config = {
-    baseURL,
-    // headers: {
-    //   Authorization: 'token 3cb476c3b0281ee28bb50f4e7e5dd0d75f4d165f',
-    // },
+    headers: {
+      Authorization: 'token 7511b53cde97b30c992118f75c57cd4d2ab65b5d',
+    },
   };
-  return axios.patch(`/gists/${id}`, data, config);
+  return instance.patch(`/gists/${id}`, data, config);
 };
 
 export const createGist = (title, description, content) => {
@@ -50,10 +46,9 @@ export const createGist = (title, description, content) => {
     },
   };
   const config = {
-    baseURL,
     // headers: {
     //   Authorization: 'token 3cb476c3b0281ee28bb50f4e7e5dd0d75f4d165f',
     // },
   };
-  return axios.put('/gists', data, config);
+  return instance.put('/gists', data, config);
 };
